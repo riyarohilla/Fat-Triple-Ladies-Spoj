6cb417f64d2ecadd9c69b86bd1de760a869c58a7
Riya Rohilla <44579551+riyarohilla@users.noreply.github.com>
2019-08-17 16:25:15 +0530

    Create triple.cpp

---
+++ triple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// your code here
+	long long t,n,sum;
+	cin>>t;
+	while(t--)
+	{
+	
+		cin>>n;
+		 sum=192+(n-1)*250;
+		cout<<sum<<endl;
+	}
+
+	return 0;
+}
